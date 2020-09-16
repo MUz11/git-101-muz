de2f06e84585808334fc8677a72eb45e847fdfcf
DESKTOP-MUz <60021368@up.ac.th>
2020-09-16 13:38:30 +0700

    MUz

---
+++ Ex.js
@@ -0,0 +1,16 @@
+//console.log("Hello World")
+let name = "max"
+name = 'max'
+let test ="Hello"
+name = `${test}B`   
+
+
+let arr = ['apple','banana','kiwi']
+console.log("arr : ", arr[1])
+
+let person ={
+    name: 'max',
+    age: '15'
+
+}
+console.log(person.name)
