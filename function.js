d3545ee258455ae2b7d0b8c82f91b71f888385d4
DESKTOP-MUz <60021368@up.ac.th>
2020-09-16 14:57:32 +0700

    MUz

---
+++ function.js
@@ -0,0 +1,14 @@
+// function add(a,b){
+//     return a + b
+// }
+
+// function mul(a,b){
+//     return a * b
+// }
+
+// function searchCustomer(customername) {
+//    //find some customer
+// }
+ const add = (a,b) => a + b
+
+console.log(add(2,498))
---
+++ loop.js
@@ -0,0 +1,10 @@
+
+// let temp =["apple","banana","kiwi"]
+// let newValue = temp.map((Element,idx)=>{
+//     return `${idx+1}.${Element}`
+
+// })
+// console.log(newValue)
+
+let filterMoreThanZero = [-1,2,3,56,6,-93,0,4].filter(el=>el>=0)
+console.log(filterMoreThanZero)
